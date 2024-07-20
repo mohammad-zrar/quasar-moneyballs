d2e9b81e7fdfb1f62b40d46cbbf661779cf38a72
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-07-20 16:34:19 +0300

    Feat(./): First commit the project layout is about to be done

---
+++ src/components/Nav/NavLink.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-item
+    clickable
+    class="text-white"
+    tag="a"
+    target="_blank"
+    :href="link"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'EssentialLink'
+});
+
+export interface NavLinkProps {
+  title: string;
+  link?: string;
+  icon?: string;
+};
+
+withDefaults(defineProps<NavLinkProps>(), {
+  link: '#',
+  icon: '',
+});
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-layout view="hHh lpR lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          <div class="absolute-center">
+             <q-icon name="savings" />
+         Moneyballs 
+          </div>
+         
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      class="bg-primary"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+        class="text-white"
+          header
+        >
+          Navigation 
+        </q-item-label>
+
+        <NavLink
+          v-for="link in navLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import NavLink, {NavLinkProps} from 'components/Nav/NavLink.vue';
+
+defineOptions({
+  name: 'MainLayout'
+});
+
+const navLinks: NavLinkProps[] = [
+  {
+    title: 'Docs',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
